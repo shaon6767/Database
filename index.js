e68ae35fa246572d6a884fbd8f1b7559905532c4
Shaon6767 <chowdhuryshaon70@gmail.com>
2026-07-04 02:26:04 +0600

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const express = require("express");
 const mongoose = require("mongoose");
+const Todo = require("./models/Todo");
 const app = express();
 
 mongoose
@@ -22,6 +23,27 @@ app.post("/todos", async (req, res) => {
     res.status(201).json(todo);
   } catch (err) {
     res.status(400).json({ error: err.message });
+  }
+});
+
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find().sort({ createdAt: -1 });
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      return res.status(404).json({ error: "Not found" });
+    }
+    res.json(todo);
+  } catch (err) {
+    res.status(404).json({ error: "Not found" });
   }
 });
 
